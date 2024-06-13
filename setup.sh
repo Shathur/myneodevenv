1055f25d1550cc20a02351796f725f84cf50196b
Shathur <iliaslangas@gmail.com>
2024-06-13 16:49:11 +0300

    sudo privileges on setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-cp -r ./* ~/.config/nvim/
-cp .tmux.conf ~/.tmux.conf
+sudo mkdir ~/.config/
+sudo mkdir ~/.config/nvim/
+sudo cp -r ./* ~/.config/nvim/
+sudo cp .tmux.conf ~/.tmux.conf
 
 # don't forget to add the nvim executable to path
 echo "export PATH="$PATH:/opt/nvim-linux64/bin"" >> ~/.bashrc
