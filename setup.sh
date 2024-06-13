3e72653dd58e5adacb0ec426b058fbd34716dcaf
Shathur <iliaslangas@gmail.com>
2024-06-13 16:45:24 +0300

    added tmux tpm

--- setup.sh
+++ setup.sh
@@ -5,3 +5,9 @@ cp .tmux.conf ~/.tmux.conf
 
 # don't forget to add the nvim executable to path
 echo "export PATH="$PATH:/opt/nvim-linux64/bin"" >> ~/.bashrc
+
+# To add the tmux functionality do the following:
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Reload TMUX env so TPM is sourced
+# tmux source ~/.tmux.conf
+# Now from inside a TMUX session press prefix + I to fetch the plugins
