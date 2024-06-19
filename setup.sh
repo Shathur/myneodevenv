9f4f9e8df6b373d3e7d892cb84f99dcaf7cf0d86
Shathur <iliaslangas@gmail.com>
2024-06-19 16:08:32 +0300

    added molten dependency

--- setup.sh
+++ setup.sh
@@ -11,6 +11,12 @@ echo "export PATH="$PATH:/opt/nvim-linux64/bin"" >> ~/.bashrc
 # problem with ownership while updating lazy-lock.json file is solved with
 sudo chown -R $USER ~/.config/nvim/
 
+# molten plugin needs runtime folder
+cd ~/.local/share/jupyter/
+mkdir runtime
+chmod a+w runtime
+cd 
+
 # To add the tmux functionality do the following:
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Reload TMUX env so TPM is sourced
