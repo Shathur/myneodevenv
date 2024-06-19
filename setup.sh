5c19ffe1c1b0ef27a968bd01c50cb6d4385f600a
Shathur <iliaslangas@gmail.com>
2024-06-19 13:28:33 +0300

    ls

--- setup.sh
+++ setup.sh
@@ -8,6 +8,9 @@ sudo cp .tmux.conf ~/.tmux.conf
 # don't forget to add the nvim executable to path
 echo "export PATH="$PATH:/opt/nvim-linux64/bin"" >> ~/.bashrc
 
+# problem with ownership while updating lazy-lock.json file is solved with
+sudo chown -R $USER ~/.config/nvim/
+
 # To add the tmux functionality do the following:
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Reload TMUX env so TPM is sourced
