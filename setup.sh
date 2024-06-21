65708e211a01ee533e10e4701e3c788087ac6c3d
Shathur <iliaslangas@gmail.com>
2024-06-21 12:32:05 +0000

    ownership

--- setup.sh
+++ setup.sh
@@ -1,22 +1,25 @@
 #! /bin/bash
 
-sudo mkdir ~/.config/
-sudo mkdir ~/.config/nvim/
-sudo cp -r ./* ~/.config/nvim/
-sudo cp .tmux.conf ~/.tmux.conf
-
-# don't forget to add the nvim executable to path
-echo "export PATH="$PATH:/opt/nvim-linux64/bin"" >> ~/.bashrc
-
-# problem with ownership while updating lazy-lock.json file is solved with
-sudo chown -R $USER ~/.config/nvim/
-
-# molten plugin needs runtime folder
+# sudo mkdir ~/.config/
+# sudo mkdir ~/.config/nvim/
 sudo mkdir ~/.local/share/
-sudo mkdir ~/.local/share/jupyter/
+sudo mkdir ~/.local/share/nvim/
+# sudo cp -r ./* ~/.config/nvim/
+# sudo cp .tmux.conf ~/.tmux.conf
+# 
+# # don't forget to add the nvim executable to path
+# echo "export PATH="$PATH:/opt/nvim-linux64/bin"" >> ~/.bashrc
+# 
+# # problem with ownership while updating lazy-lock.json file is solved with
+# sudo chown -R $USER ~/.config/nvim/
+sudo chown -R $USER ~/.local/share/nvim/
+# 
+# # molten plugin needs runtime folder
+# sudo mkdir ~/.local/share/
+# sudo mkdir ~/.local/share/jupyter/
 cd ~/.local/share/jupyter/
-mkdir runtime
-chmod a+w runtime
+sudo mkdir runtime
+sudo chmod a+w runtime
 cd 
 
 # To add the tmux functionality do the following:
