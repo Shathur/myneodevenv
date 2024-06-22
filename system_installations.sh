24ec0c3270299cb21c587bf7a843dbff9d6e524d
Shathur <iliaslangas@gmail.com>
2024-06-22 08:42:13 +0000

    necessary install for building in barebone images

--- system_installations.sh
+++ system_installations.sh
@@ -4,6 +4,8 @@
 sudo apt update &&
 sudo apt upgrade -y &&
 
+sudo apt install file -y && # some ubuntu images are barebone, and need this for building from source
+
 # reason we are here -- build from source, cause arm64
 # code taken from here https://github.com/neovim/neovim/blob/master/BUILD.md
 # dependencies first
