4bab622fdfbe1d1fb5ca2e87ef286514844e09f1
Shathur <iliaslangas@gmail.com>
2024-12-07 19:49:50 +0200

    some explanation for passphrase usage for gpt addon

--- lua/shathur/plugins/chatgpt.lua
+++ lua/shathur/plugins/chatgpt.lua
@@ -11,6 +11,8 @@ return {
 		local home = vim.fn.expand("$HOME")
 		require("chatgpt").setup({
 			-- this thing doesn't work if you don't give the first / in /.config/openaiapikey.txt.gpg
+			-- if you add a passphrase add it after --decrypt and care with the space after the password
+			-- " --passphrase your_password ", always leave a space between your password and the next "
 			api_key_cmd = "gpg --decrypt " .. home .. "/.config/openaiapikey.txt.gpg",
 		})
 	end,
